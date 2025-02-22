66d8dea0bd8574fa3c313cc3a8d1e2bc68c03a38
Hudson Cole <72815680+net-hex@users.noreply.github.com>
2025-02-22 14:58:55 -0600

    Add descriptions
    Decided to add descriptions to everything because it's not instantly obvious what this does at first glance

--- lcg.py
+++ lcg.py
@@ -1,6 +1,16 @@
+## This program is a linear congruency generator, or LCG
+## This will generate a series of pseudo-random numbers
+## This is not made to be cryptographically secure, it is more of a practical test
+## That being said, use it if you like
+
+
+## A hardcoded start value
 seed = 3
+## The multiplier. This changes how previous values influence the next in sequence
 a = 5
+## The increment, for added "randomness"
 b = 13
+## The modulus, which defines the range of possible values
 m = 32
 
 values_gen = [seed]
